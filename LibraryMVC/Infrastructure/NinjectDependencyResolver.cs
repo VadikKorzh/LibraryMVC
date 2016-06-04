408bb2c596f4c03c41988d50e81d4248a79b5e90
VadikKorzh <Kharkiv@brainacad.com>
2016-06-04 11:31:50 +0300

    Started adding Ninject

---
+++ LibraryMVC/Infrastructure/NinjectDependencyResolver.cs
@@ -0,0 +1,30 @@
+﻿using Ninject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LibraryMVC.Infrastructure
+{
+    public class NinjectDependencyResolver : IDependencyResolver
+    {
+        private IKernel _kernel;
+
+        public NinjectDependencyResolver()
+        {
+            _kernel = new StandardKernel();
+            AddBindings();
+        }
+
+        public object GetService(Type serviceType)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
